6640c626464fc708a842d7e298b572535e0924a3
KirillSukhorukikh <kirill.su@tenengroup.com>
2022-11-20 01:08:35 +0600

    added swagger

--- SimonsVoss/SimonsVoss/Program.cs
+++ SimonsVoss/SimonsVoss/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = builder.Environment.ApplicationName,
+        Version = "v1" });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json",
+        $"{builder.Environment.ApplicationName} v1"));
+}
 
+app.MapGet("/", () => "Hello Swagger!");
 app.Run();
